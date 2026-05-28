6426383bea8962c65eb62ea93ecddcf58e1cf17b
Vesanmartin <ve.sanmartin@duocuc.cl>
2026-05-27 21:10:23 -0400

    Update API endpoint in TableDespachos component

--- src/componentes/CrudAdmin/TableDespachos.jsx
+++ src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://98.93.120.147:8081/api/v1/despachos", {
+      ..get("/api/v1/despachos",  {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
